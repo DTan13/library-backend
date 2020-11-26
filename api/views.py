5d9eeb6df97afc800e66c8dc5c5c9cc663d5b3ca
Dhananjay Tanpure <dtan13@pm.me>
2020-11-26 22:41:38 +0530

    Added URL mapping and views for Book model
    mapping /api/books/

--- api/views.py
+++ api/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from api.models import Book
 
 
 def index(request):
-    return JsonResponse([{"status": "Working"}], safe=False)
+    return JsonResponse({"status": "Working"}, safe=False)
+
+
+@csrf_exempt
+def books(request):
+    body_data = 0
+    try:
+        body_unicode = request.body.decode('utf-8')
+        body_data = json.loads(body_unicode)
+    except json.decoder.JSONDecodeError as DecodeError:
+        print(DecodeError)
+
+    if request.method == 'POST':
+        result = Book.SaveBook(body_data)
+        return HttpResponse(result)
+
+    if request.method == 'GET':
+        data = Book.GetBooks()
+        return JsonResponse(data, safe=False)
