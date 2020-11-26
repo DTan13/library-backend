c4661ff8b9e50cfcbed4fc3b843d847364381145
Dhananjay Tanpure <dtan13@pm.me>
2020-11-26 22:29:45 +0530

    Added basic connection to database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from pymongo import errors
+import pymongo
 
-# Create your models here.
+try:
+    # Make sure MongoDB is running on port 27017
+    # If you are having problems with "localhost" try "mongodb://127.0.0.1:27017/"
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+except errors.ConnectionFailure as ConnectionError:
+    print(ConnectionError.message)
+
+backendDB = client['te-project-backend-db']
+print(client.list_database_names())
