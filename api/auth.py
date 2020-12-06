9c6276b3f2777116e7e64431d07ffad99e8c2ada
Dhananjay Tanpure <dtan13@pm.me>
2020-12-06 14:24:44 +0530

    Basic signup Method

--- api/auth.py
+++ api/auth.py
@@ -1,2 +1,20 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from api.models import User
 
 
+
+@csrf_exempt
+def signup(request):
+    """
+    This method is for user to signup
+    """
+    user_body_data = 0
+    try:
+        body_unicode = request.body.decode('utf-8')
+        user_body_data = json.loads(body_unicode)
+    except json.decoder.JSONDecodeError as DecodeError:
+        print(DecodeError)
+
+    return HttpResponse(User.SaveUser(user_body_data))
