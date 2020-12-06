df14b74fca62f57b38238c1a7939ae7aeb53ffa3
Dhananjay Tanpure <dtan13@pm.me>
2020-12-06 14:25:09 +0530

    Basic login method

--- api/auth.py
+++ api/auth.py
@@ -4,6 +4,19 @@ from django.http import HttpResponse
 from api.models import User
 
 
+@csrf_exempt
+def login(request):
+    """
+    This method is for user to login
+    """
+    user_body_data = 0
+    try:
+        body_unicode = request.body.decode('utf-8')
+        user_body_data = json.loads(body_unicode)
+    except json.decoder.JSONDecodeError as DecodeError:
+        print(DecodeError)
+
+    return HttpResponse(User.CheckUser(user_body_data))
 
 @csrf_exempt
 def signup(request):
